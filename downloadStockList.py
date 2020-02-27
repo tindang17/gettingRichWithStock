18a72dab664a137840dcdecc939c5bb02df28775
Steve <stevennhnk113@gmail.com>
2020-02-26 19:44:18 -0800

    initial commit

---
+++ downloadStockList.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import csv
+
+
+def DownloadStockList():
+    stockList = []
+    for i in range(1, 2):
+        url = "https://www.nasdaq.com/api/v1/screener?page=" + \
+            str(i) + "&pageSize=300"
+
+        payload = {}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        data = json.loads(response.text)
+
+        for stock in data['data']:
+            temp = []
+            temp.append(stock['ticker'])
+            stockList.append(temp)
+
+    with open('stockList.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(stockList)
+    print()
+
+
+DownloadStockList()
