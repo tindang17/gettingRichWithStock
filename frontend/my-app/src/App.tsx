210e4e6de4a41b68dc36ea7ce2c5869fba7097e9
Tin Dang <tindang1710@gmail.com>
2020-03-04 00:05:15 -0800

    update app.tsx

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get rich with stock
         </a>
       </header>
     </div>
