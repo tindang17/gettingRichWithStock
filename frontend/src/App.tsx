5284ac6209462b40f4089ad8bb00a7af777b5146
Tin Dang <tindang1710@gmail.com>
2020-03-10 00:03:49 -0700

    add formidable/victory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Time extends React.Component<Props, State> {}
 function App() {
   return (
     <div className="App">
